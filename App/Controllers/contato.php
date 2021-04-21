d9f2b82932d9a81592c9ad5348a8ba6213f136d1
sandrosa1 <sandrosa@fatec.sp.gov.br>
2021-04-20 21:54:23 -0300

    Rota e URL amigavel

---
+++ App/Controllers/contato.php
@@ -0,0 +1,16 @@
+<?php
+
+class Contato{
+
+    public function index(){
+        echo 'index de contato';
+    }
+
+    public function email(){
+        echo 'Metodo email';
+    }
+
+    public function telefone(){
+        echo '(11)9422-13409';
+    }
+}
--- App/Core/App.php
+++ App/Core/App.php
@@ -4,7 +4,56 @@ namespace App\Core;
 
 class App {
 
+    protected $controller  = 'home';
+    protected $method      = 'index';
+    protected $params      = [];
+
+    /**
+     * Responsavel pela criação das rotas e URL amigaveis
+     */
     function __construct(){
-        echo "Teste PHP";
+        print_r($url = $this->parseURL()) ;
+
+        /**
+         * Se existir o atributo, controler recebe o atributo
+         */
+        if(file_exists('../App/Controllers/'.$url[1].'.php')){
+            $this->controller = $url[1];
+            unset($url[1]);
+        }
+
+        require_once '../App/Controllers/'.$this->controller.'.php';
+        $this->controller = new $this->controller;
+
+        /**
+         *se existe o método dentro do controller, method recebe o método
+         */
+        if(isset($url[2])){
+            if(method_exists($this->controller,$url[2])){
+                $this->method = $url[2];
+                unset($url[2]);
+                unset($url[0]);
+            }
+        }
+
+        /**
+         * Recebe os parametros do indíce 3 em diante
+         */
+        $this->params = $url ? array_values($url) : [];
+
+        /**
+         * Executa um método que está dentro de um controller, recebido pela URL
+         */
+        call_user_func_array([$this->controller, $this->method], $this->params);
+
+    }
+
+    /**
+     * Método responsável por capturar o caminho da url
+     *
+     * @return array
+     */
+    public function parseURL(){
+        return explode ('/',filter_var($_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI'],FILTER_SANITIZE_URL));
     }
 }
